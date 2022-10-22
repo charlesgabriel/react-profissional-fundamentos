a5dadfbb53600f69f9d227879e5b27053401e254
Charles Gabriel L Carmo <charlesgabriel@hotmail.com.br>
2022-10-22 16:47:51 -0300

    iniciado o hero/started the hero

---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Hero = () => (
+    <div>
+        <div>
+            <h1>Ganhe sua liberdade para ir e vir</h1>
+            <ul>
+                <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum, similique labore. Sapiente fugiat sit nisi obcaecati quos labore explicabo, voluptatum eaque, tempora quam dolore adipisci ad aut exercitationem tempore pariatur.</li>
+                <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum, similique labore. Sapiente fugiat sit nisi obcaecati quos labore explicabo, voluptatum eaque, tempora quam dolore adipisci ad aut exercitationem tempore pariatur.</li>
+                <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum, similique labore. Sapiente fugiat sit nisi obcaecati quos labore explicabo, voluptatum eaque, tempora quam dolore adipisci ad aut exercitationem tempore pariatur.</li>
+            </ul>
+            <button>Matricule-se agora</button>
+        </div>
+    </div>
+);
+
+export default Hero;
