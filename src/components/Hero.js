58aaae09df2449c2692b6691441ae9ac586a1100
Charles Gabriel L Carmo <charlesgabriel@hotmail.com.br>
2022-10-22 17:16:37 -0300

    adicionado composição de componentes/added components composition

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,17 +1,12 @@
-import React from 'react';
+import React from "react";
 
-const Hero = () => (
+const Hero = ({ title, children }) => (
+  <div>
     <div>
-        <div>
-            <h1>Ganhe sua liberdade para ir e vir</h1>
-            <ul>
-                <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum, similique labore. Sapiente fugiat sit nisi obcaecati quos labore explicabo, voluptatum eaque, tempora quam dolore adipisci ad aut exercitationem tempore pariatur.</li>
-                <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum, similique labore. Sapiente fugiat sit nisi obcaecati quos labore explicabo, voluptatum eaque, tempora quam dolore adipisci ad aut exercitationem tempore pariatur.</li>
-                <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum, similique labore. Sapiente fugiat sit nisi obcaecati quos labore explicabo, voluptatum eaque, tempora quam dolore adipisci ad aut exercitationem tempore pariatur.</li>
-            </ul>
-            <button>Matricule-se agora</button>
-        </div>
+      <h1>{title}</h1>
+      {children}
     </div>
+  </div>
 );
 
 export default Hero;
--- src/stories/Hero.stories.js
+++ src/stories/Hero.stories.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React from "react";
 
-import Hero from '../components/Hero';
+import Hero from "../components/Hero";
 
 export default {
-    title: "Hero",
-    component: Hero,   
+  title: "Hero",
+  component: Hero,
 };
 
-export const usage = () => <Hero />;
+export const usage = () => (
+  <Hero title="Ganhe sua liberdade para ir e vir">
+    <p>A auto escola lider em aprovação.</p>
+  </Hero>
+);
+
+export const withList = () => (
+  <Hero title="Ganhe sua liberdade para ir e vir">
+    <ul>
+      <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</li>
+      <li>Earum, similique labore.</li>
+      <li>
+        Sapiente fugiat sit nisi obcaecati quos labore explicabo, voluptatum
+        eaque, tempora quam dolore adipisci ad aut exercitationem tempore
+        pariatur.
+      </li>
+    </ul>
+    <button>Matricule-se agora</button>
+  </Hero>
+);
