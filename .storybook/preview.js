c424c58d57ecefab489ba1e6100714ca6f696213
Charles Gabriel L Carmo <charlesgabriel@hotmail.com.br>
2022-10-24 20:04:52 -0300

    implementado testes em diferentes tamanhos de telas/implemented tests on different screen sizes

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,45 @@
-import { addDecorator } from "@storybook/react";
+import React from "react";
+import { addDecorator, addParameters } from "@storybook/react";
 
 import GlobalStyle from "../src/components/GlobalStyle";
+
+const viewports = {
+  extraSmall: {
+    name: "Portrait phone (default)",
+    styles: {
+      width: "360px",
+      height: "640px",
+    },
+  },
+  small: {
+    name: "Landscap phone (sm)",
+    styles: {
+      width: "640px",
+      height: "360px",
+    },
+  },
+  medium: {
+    name: "Tablet (md)",
+    styles: {
+      width: "768px",
+      height: "1024px",
+    },
+  },
+  large: {
+    name: "Desktop (lg)",
+    styles: {
+      width: "1024px",
+      height: "1366px",
+    },
+  },
+  extraLarg: {
+    name: "Large Desktop (xl)",
+    styles: {
+      width: "1280px",
+      height: "800px",
+    },
+  },
+};
 
 addDecorator((storyFn) => (
   <>
@@ -8,6 +47,12 @@ addDecorator((storyFn) => (
     {storyFn()}
   </>
 ));
+
+addParameters({
+  viewport: {
+    viewports,
+  },
+});
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
