8fdabb8a992bec38f93e2088599caf74e8656498
Charles Gabriel L Carmo <charlesgabriel@hotmail.com.br>
2022-10-27 21:15:02 -0300

    criado documentação do heading/created heading documentation

---
+++ src/stories/Heading.stories.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import Heading from "../components/Heading";
+
+export default {
+  title: "Heading",
+  component: Heading,
+};
+
+export const usage = () => (
+  <div>
+    <Heading>
+      <h1>Título</h1>
+    </Heading>
+    <Heading>
+      <h2>Título</h2>
+    </Heading>
+    <Heading>
+      <h3>Título</h3>
+    </Heading>
+    <Heading>
+      <h4>Título</h4>
+    </Heading>
+    <Heading>
+      <h5>Título</h5>
+    </Heading>
+    <Heading>
+      <h6>Título</h6>
+    </Heading>
+  </div>
+);
