90a844c5348583ee9ee8548532c7cc5c16712600
Charles Gabriel L Carmo <charlesgabriel@hotmail.com.br>
2022-10-30 14:19:16 -0300

    criado um teste de unidade para o componente hero/creted a unit test for the hero component

---
+++ src/components/Hero.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Hero from "./Hero";
+
+test("render Hero with children", () => {
+  const { getByText } = render(
+    <Hero>
+      <p>Charles Gabriel</p>
+    </Hero>
+  );
+
+  expect(getByText("Charles Gabriel")).toBeTruthy();
+});
