14d8b2a62e9f4eefdd5432d9f21f50c345055950
Charles Gabriel L Carmo <charlesgabriel@hotmail.com.br>
2022-11-02 09:33:47 -0300

    consertado a font-family utilizada no storybook/fixed the font-family used in the storybook

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,4 +1,6 @@
+import React from "react";
 import { createGlobalStyle } from "styled-components";
+import { Helmet } from "react-helmet";
 
 import { breakAt, BreakpointSizes } from "./Breakpoints";
 
@@ -81,4 +83,18 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-export default GlobalStyle;
+const GlobalStyleComposed = () => (
+  <>
+    <GlobalStyle />
+    <Helmet>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600;700&display=swap"
+        rel="stylesheet"
+      />
+    </Helmet>
+  </>
+);
+
+export default GlobalStyleComposed;
