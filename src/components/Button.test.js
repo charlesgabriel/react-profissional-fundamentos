720c7109903bf7cb91707abf4af504bb5fcca7d9
Charles Gabriel L Carmo <charlesgabriel@hotmail.com.br>
2022-11-08 22:46:57 -0300

    adicionado teste no evento de clique no componente botão/added test in button component click event

--- src/components/Button.test.js
+++ src/components/Button.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "test-utils";
+import { render, fireEvent } from "test-utils";
 
 import Button from "./Button";
 
@@ -7,4 +7,11 @@ test("renders a text", () => {
   const { getByText } = render(<Button>Click here</Button>);
 
   expect(getByText("Click here")).toBeInTheDocument();
+});
+
+test("trigger event on click", () => {
+  const handleClick = jest.fn();
+  const { getByRole } = render(<Button onClick={handleClick} />);
+  fireEvent.click(getByRole("button"));
+  expect(handleClick).toBeCalled();
 });
