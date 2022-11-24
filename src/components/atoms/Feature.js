b1695ef9c7886988a8df02d5f71f1089806cc046
Charles Gabriel L Carmo <charlesgabriel@hotmail.com.br>
2022-11-24 16:40:11 -0300

    gerado componente a partir do code snippet criado/generated component from the created code snippet

---
+++ src/components/atoms/Feature.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Feature = () => (
+  
+);
+
+Feature.defaultProps = {};
+
+Feature.PropTypes = {};
+
+export default Feature;
