463c89f5ce13858adc33afab6c084e4516525c57
Charles Gabriel L Carmo <charlesgabriel@hotmail.com.br>
2022-11-28 21:57:03 -0300

    construido grid para mostrar alguns componentes feature/built grid to show some feature components

--- src/components/atoms/Feature.stories.js
+++ src/components/atoms/Feature.stories.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { FaCar } from "react-icons/fa";
+import { FaCar, FaKey, FaMapMarkedAlt, FaAccessibleIcon } from "react-icons/fa";
 
+import Grid from "components/atoms/Grid";
 import Feature from "./Feature";
 
 export default {
@@ -12,4 +13,21 @@ export const usage = () => (
   <Feature icon={<FaCar />} title="Título">
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
   </Feature>
+);
+
+export const withGrid = () => (
+  <Grid sm={2} md={4}>
+    <Feature icon={<FaCar />} title="Maior e melhor fronta">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </Feature>
+    <Feature icon={<FaKey />} title="Habilitação mais rápida">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </Feature>
+    <Feature icon={<FaMapMarkedAlt />} title="Excelente localização">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </Feature>
+    <Feature icon={<FaAccessibleIcon />} title="Acessibilidade">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </Feature>
+  </Grid>
 );
