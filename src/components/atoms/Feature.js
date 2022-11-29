aa5d78ead867be15f0d0f61c839231ce197c21cd
Charles Gabriel L Carmo <charlesgabriel@hotmail.com.br>
2022-11-28 21:27:56 -0300

    definido o estilo do ícone/defined icon style

--- src/components/atoms/Feature.js
+++ src/components/atoms/Feature.js
@@ -2,6 +2,8 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
+const getPrimaryColor = ({ theme }) => theme.colors.primary.main;
+
 const Root = styled.div`
   text-align: center;
 
@@ -14,9 +16,40 @@ const Root = styled.div`
   }
 `;
 
+const IconRoot = styled.div`
+  border-radius: 50%;
+  display: inline-block;
+  height: 116px;
+  padding: 8px;
+  width: 116px;
+
+  &:hover {
+    border: 1px solid ${getPrimaryColor};
+
+    > * {
+      background-color: ${getPrimaryColor};
+      color: #fff;
+    }
+  }
+`;
+
+const IconContainer = styled.div`
+  align-items: center;
+  border: 1px solid #e9e9e9;
+  border-radius: 50%;
+  color: ${getPrimaryColor};
+  display: flex;
+  font-size: 32px;
+  height: 100%;
+  justify-content: center;
+  width: 100%;
+`;
+
 const Feature = ({ icon, title, children }) => (
   <Root>
-    <div>{icon}</div>
+    <IconRoot>
+      <IconContainer>{icon}</IconContainer>
+    </IconRoot>
     <h5>{title}</h5>
     <div>{children}</div>
   </Root>
