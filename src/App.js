67291ec15fdae3f8c9eab633e6f4bb0853ca00cb
Charles Gabriel L Carmo <charlesgabriel@hotmail.com.br>
2022-12-10 15:20:28 -0300

    criado o componente home com a metodologia atomic design/created the home component with the atomic design methodology

--- src/App.js
+++ src/App.js
@@ -1,54 +1,15 @@
 import React from "react";
-import { FaCar, FaKey, FaMapMarkedAlt, FaAccessibleIcon } from "react-icons/fa";
 
-import Hero from "components/molecules/Hero";
-import Heading from "components/atoms/Heading";
-import Button from "components/atoms/Button";
 import ThemeProvider from "styles/ThemeProvider";
 import GlobalStyle from "styles/GlobalStyle";
-import Grid from "components/atoms/Grid";
-import Feature from "components/atoms/Feature";
 
-import bgCarImage from "stories/assets/bg-car.jpg";
+import Home from "components/pages/Home";
 
 function App() {
   return (
     <ThemeProvider>
       <GlobalStyle />
-      <Hero image={bgCarImage}>
-        <Heading>
-          <h1>
-            Ganhe sua <strong>liberdade</strong>
-            <br /> para ir e vir
-          </h1>
-        </Heading>
-        <ul>
-          <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</li>
-          <li>Earum, similique labore.</li>
-          <li>
-            Sapiente fugiat sit nisi obcaecati quos labore explicabo, voluptatum
-            eaque, tempora quam dolore adipisci ad aut exercitationem tempore
-            pariatur.
-          </li>
-        </ul>
-        <Button color="primary" variant="outlined">
-          Matricule-se agora
-        </Button>
-      </Hero>
-      <Grid sm={2} md={4}>
-        <Feature icon={<FaCar />} title="Maior e melhor fronta">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        </Feature>
-        <Feature icon={<FaKey />} title="Habilitação mais rápida">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        </Feature>
-        <Feature icon={<FaMapMarkedAlt />} title="Excelente localização">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        </Feature>
-        <Feature icon={<FaAccessibleIcon />} title="Acessibilidade">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        </Feature>
-      </Grid>
+      <Home />
     </ThemeProvider>
   );
 }
---
+++ src/components/pages/Home.js
@@ -0,0 +1,56 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { FaCar, FaKey, FaMapMarkedAlt, FaAccessibleIcon } from "react-icons/fa";
+
+import Hero from "components/molecules/Hero";
+import Heading from "components/atoms/Heading";
+import Button from "components/atoms/Button";
+import Grid from "components/atoms/Grid";
+import Feature from "components/atoms/Feature";
+
+import bgCarImage from "stories/assets/bg-car.jpg";
+
+const Home = () => (
+  <>
+    <Hero image={bgCarImage}>
+      <Heading>
+        <h1>
+          Ganhe sua <strong>liberdade</strong>
+          <br /> para ir e vir
+        </h1>
+      </Heading>
+      <ul>
+        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</li>
+        <li>Earum, similique labore.</li>
+        <li>
+          Sapiente fugiat sit nisi obcaecati quos labore explicabo, voluptatum
+          eaque, tempora quam dolore adipisci ad aut exercitationem tempore
+          pariatur.
+        </li>
+      </ul>
+      <Button color="primary" variant="outlined">
+        Matricule-se agora
+      </Button>
+    </Hero>
+    <Grid sm={2} md={4}>
+      <Feature icon={<FaCar />} title="Maior e melhor fronta">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      </Feature>
+      <Feature icon={<FaKey />} title="Habilitação mais rápida">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      </Feature>
+      <Feature icon={<FaMapMarkedAlt />} title="Excelente localização">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      </Feature>
+      <Feature icon={<FaAccessibleIcon />} title="Acessibilidade">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      </Feature>
+    </Grid>
+  </>
+);
+
+Home.defaultProps = {};
+
+Home.PropTypes = {};
+
+export default Home;
