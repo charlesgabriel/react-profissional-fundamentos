648c66f1dabb79fed55baa7612f8eb7a421e7328
Charles Gabriel L Carmo <charlesgabriel@hotmail.com.br>
2022-12-26 21:14:28 -0300

    adicionado itens no rodapé/added items in footer

--- src/components/organisms/Footer.js
+++ src/components/organisms/Footer.js
@@ -1,5 +1,12 @@
 import React from "react";
 import styled from "styled-components";
+import {
+  FaPhone,
+  FaBuilding,
+  FaFacebookF,
+  FaLinkedinIn,
+  FaTwitter,
+} from "react-icons/fa";
 
 import Container from "components/atoms/Container";
 import Grid from "components/atoms/Grid";
@@ -13,6 +20,20 @@ const Root = styled.footer`
   h6 {
     color: #fff;
     margin-top: 0;
+    text-transform: uppercase;
+  }
+`;
+
+const IconContainer = styled.span`
+  margin-right: 8px;
+`;
+
+const FooterLink = styled.a`
+  color: ${(props) => props.theme.colors.primary.main};
+  text-decoration: none;
+
+  &:hover {
+    color: ${(props) => props.theme.colors.primary.dark};
   }
 `;
 
@@ -24,16 +45,67 @@ const Footer = () => (
           <Heading>
             <h6>Auto Escola</h6>
           </Heading>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad adipisci
+            necessitatibus, dignissimos facere, quisquam tempore eum corrupti
+            distinctio quas error corporis a deserunt repudiandae aliquid fugiat
+            perspiciatis velit, voluptatibus doloremque?
+          </p>
         </div>
         <div>
           <Heading>
             <h6>Contatos</h6>
           </Heading>
+          <p>
+            <IconContainer>
+              <FaPhone />
+            </IconContainer>
+            (12) 99876-5432
+          </p>
+          <p>
+            <IconContainer>
+              <FaBuilding />
+            </IconContainer>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
         </div>
         <div>
           <Heading>
             <h6>Redes sociais</h6>
           </Heading>
+          <p>
+            <FooterLink
+              target="_blank"
+              href="https://www.facebook.com/nardiniacademy/"
+            >
+              <IconContainer>
+                <FaFacebookF />
+              </IconContainer>
+              Facebook
+            </FooterLink>
+          </p>
+          <p>
+            <FooterLink
+              target="_blank"
+              href="https://www.linkedin.com/company/nardini-academy"
+            >
+              <IconContainer>
+                <FaLinkedinIn />
+              </IconContainer>
+              LinkedIn
+            </FooterLink>
+          </p>
+          <p>
+            <FooterLink
+              target="_blank"
+              href="https://twitter.com/nardiniacademy"
+            >
+              <IconContainer>
+                <FaTwitter />
+              </IconContainer>
+              twitter
+            </FooterLink>
+          </p>
         </div>
       </Grid>
     </Container>
