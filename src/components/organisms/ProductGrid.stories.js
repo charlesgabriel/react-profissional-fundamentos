7eeac6dda20b57c0dba9b7fcc21682893ad81a46
Charles Gabriel L Carmo <charlesgabriel@hotmail.com.br>
2023-02-11 12:34:26 -0300

    definido a imagem do productgrid/defined the image of the productgrid

--- src/components/organisms/ProductGrid.stories.js
+++ src/components/organisms/ProductGrid.stories.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import ProductGrid from "./ProductGrid";
+import PlaceholderImage from "stories/assets/transit.jpg";
 
 export default {
   title: "Components/Organisms/ProductGrid",
@@ -10,9 +11,12 @@ export default {
 export const usage = () => (
   <ProductGrid
     products={[
-      { id: 1, title: "Título", summary: "Texto" },
-      { id: 2, title: "Título", summary: "Texto" },
-      { id: 3, title: "Título", summary: "Texto" },
+      { id: 1, image: PlaceholderImage, title: "Título 1", summary: "Texto" },
+      { id: 2, image: PlaceholderImage, title: "Título 2", summary: "Texto" },
+      { id: 3, image: PlaceholderImage, title: "Título 3", summary: "Texto" },
+      { id: 4, image: PlaceholderImage, title: "Título 4", summary: "Texto" },
+      { id: 5, image: PlaceholderImage, title: "Título 5", summary: "Texto" },
+      { id: 6, image: PlaceholderImage, title: "Título 6", summary: "Texto" },
     ]}
   />
 );
