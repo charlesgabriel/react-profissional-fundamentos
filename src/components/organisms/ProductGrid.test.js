c12ec0a39aa5884bfc1db39d0f071ed02773b480
Charles Gabriel L Carmo <charlesgabriel@hotmail.com.br>
2023-02-13 21:10:22 -0300

    criado o teste de exibição de componente/created the component display test

---
+++ src/components/organisms/ProductGrid.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { render, screen } from "test-utils";
+
+import ProductGrid from "./ProductGrid";
+
+const buildProducts = (size) => {
+  const result = [];
+
+  for (let i = 0; i < size; i++) {
+    result.push({ id: i, title: `Title ${i}` });
+  }
+
+  return result;
+};
+
+test("with 6 products show only 3 items", () => {
+  render(<ProductGrid products={buildProducts(6)} />);
+
+  expect(screen.getAllByRole("heading").length).toBe(3);
+});
